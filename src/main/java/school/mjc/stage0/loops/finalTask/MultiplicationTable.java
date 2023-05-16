c412823c2dea00a6f08017c31130ebc64e1873b6
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-17 01:55:29 +0600

    fixfix2mod4taskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -4,7 +4,7 @@ public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
         for (int i = 1; i <= numberTableToPrint; i++) {
                 System.out.print("" + i + " * " + numberTableToPrint + " = " + i * numberTableToPrint);
-            }
+            
             System.out.println();
         }
     }
