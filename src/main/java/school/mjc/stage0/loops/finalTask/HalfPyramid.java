a4cbf942377fba4d69830633902fe8fb38bd4a4b
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-17 01:50:49 +0600

    mod4taskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for(int i = 1; i <= cathetusLength; i++) {
+            for(int j = cathetusLength; j > i; j--)
+                System.out.print(' ');
+            for(int j = i; j >= 1; j--) System.out.print('*');
+            //for(int j = 2; j <= i; j++) System.out.print(j);
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        for (int i = 1; i <= numberTableToPrint; i++) {
+            for (int j = 1; j <= numberTableToPrint; j++) {
+                System.out.print("" + i * j + " ");
+            }
+            System.out.println();
+        }
     }
 }
