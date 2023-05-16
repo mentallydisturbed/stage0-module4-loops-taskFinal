cc59d59fce96b4d02363d8574a9e1dd730bf68bf
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-17 01:57:21 +0600

    fixed

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
-        for (int i = 1; i <= numberTableToPrint; i++) {
+        for (int i = 1; i <= 10; i++) {
                 System.out.print("" + i + " * " + numberTableToPrint + " = " + i * numberTableToPrint);
-            
+
             System.out.println();
         }
     }
